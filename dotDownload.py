67fd8b001341ab568cb67c24591715a35ece2dad
reversee <rare7989@gmail.com>
2021-12-02 00:12:19 +0100

    init

---
+++ dotDownload.py
@@ -0,0 +1,53 @@
+from pynput import keyboard
+from dotSetup import install
+from clint.textui import progress
+import time, os
+import requests
+import json
+
+appversion = "v1.1b"
+appstable = "unstable"
+
+def download(host, filename):
+    if (not os.path.isfile(filename)): 
+        r = requests.get(host, stream=True)
+        path = filename
+        with open(path, 'wb') as f:
+            tlength = int(r.headers.get('content-length'))
+            for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(tlength/1024) +1):
+                if chunk:
+                    f.write(chunk) 
+                    f.flush()
+    else:
+        print("File already exists, do you want to delete and redownload it? (y/N)")
+        time.sleep(1)
+        with keyboard.Events() as events:
+            event = events.get(1e6)
+            if (event.key) == keyboard.KeyCode.from_char('y'): os.remove(filename); download(host, filename); 
+            else: exit()
+
+print(" dotDownload | " + appversion + " | " + appstable + " | " )
+metadataFile = input("Drag and drop metadata file here: "); f = open(metadataFile)
+metadataParsed = json.load(f)
+f.close()
+
+print("\nLoaded file: " + metadataFile)
+print(" Game title: " + metadataParsed["title"])
+print(" Original Size: " + metadataParsed["originalsize"])
+print(" Repack Size: " + metadataParsed["repacksize"])
+print(" Setup type: " + metadataParsed["setup"])
+print(" Other info: " + metadataParsed["comment"])
+
+print("\nWhat to do?")
+print(" i - install ")
+print(" d - download ")
+print(" q - exit\n")
+time.sleep(1)
+with keyboard.Events() as events:
+    event = events.get(1e6)
+    if (event.key) == keyboard.KeyCode.from_char('d'): download(metadataParsed["host"], metadataParsed["archivename"]); 
+    if (event.key) == keyboard.KeyCode.from_char('i'): install(metadataParsed["setup"], metadataParsed["archivename"], metadataParsed["title"], metadataParsed["executable"]); 
+    else: exit()
+
+
+
---
+++ dotSetup.py
@@ -0,0 +1,29 @@
+from clint.textui import puts, colored, prompt, validators
+import time, os
+
+
+def install(stype, filename, gname, gexec):
+    if not os.path.exists(filename):
+        print("You must download game archive! select d after selecting metadata!")
+
+    if (stype) == 'unpack': 
+        puts('Installing using: ' + colored.red('unpack')); time.sleep(0.2)
+        outpath = prompt.query('Installation Path ( no spaces )', default='C:\dotGames\\'+filename.replace('.arc', ''))
+        if (not os.path.exists(outpath)):
+            os.makedirs(outpath)
+        unpack(filename, outpath)
+        postInstall(gname, gexec, outpath)
+
+    if (stype) == 'dotSetup': print("not implemented")
+
+def unpack(filename, outpath): 
+    print("\n")
+    os.system('arc.exe x -o+ '+ filename + ' --yes --display --diskpath' + outpath.replace(' ', '') )
+    
+def postInstall(gname, gexec, outpath):
+    puts("\n " + colored.green("Creating Desktop Shortcuts"))
+
+    desktop = "%userprofile%\Desktop\\"
+    d2 = '"'+desktop+gname+'" '
+    os.system('mklink ' +d2 + outpath + '\\' + gexec)
+    
